308faf6a0d3ae0454b35579e9f7bb9de76ee3b84
chenop <chenop@gmail.com>
2017-10-13 21:37:36 +0300

    remove api

--- src/snippets.js
+++ src/snippets.js
@@ -5,3 +5,32 @@ const log = ({getState, dispatch}) => next => action => {
 };
 
 export default log;
+
+const api = ({ dispatch, getState }) => next => action => {
+
+	if (action.type !== actions.API) {
+		return next(action);
+	}
+
+	const { url, success, schema, label } = action.payload;
+
+	dispatch(startNetwork(label));
+
+	fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			if (schema) {
+				data = normalize(data, schema);
+			}
+
+			dispatch(success(data));
+
+			dispatch(endNetwork(label));
+		})
+		.catch(error => {
+			console.error(error);
+			dispatch(endNetwork(label))
+		})
+};
+
+export default api;
